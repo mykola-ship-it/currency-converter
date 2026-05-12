d1d0760af0f21ee61c5ada4fc99a4cfd78f31d89
mykola-ship-it <mykola.vovchuk.24@pnu.edu.ua>
2026-05-12 20:39:09 +0300

    Refactor currency conversion script
    Updated currency conversion logic and variable names.

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import requests
 
-S = input("Введіть суму: ")
-Currency = input("Введіть валюту: ")
-ToCurrency = input("В яку валюту конвертувати: ")
+amount = input("Введіть суму: ")
+from_currency = input("Введіть валюту (наприклад UAH): ").upper()
+to_currency = input("В яку валюту конвертувати (наприклад USD): ").upper()
 
-Url = f"https://api.frankfurter.app/latest?amount={S}&from={Currency}&to={ToCurrency}"
+url = f"https://api.frankfurter.app/v2/rates?amount={amount}&from={from_currency}&to={to_currency}"
 
-response = requests.get(Url)
+print(url)
+
+response = requests.get(url)
 
 if response.status_code == 200:
     data = response.json()
 
-    result = data["rates"][ToCurrency]
+    result = data["rates"][to_currency]
 
     print("Результат:")
-    print(S, Currency, "=", result, ToCurrency)
+    print(amount, from_currency, "=", result, to_currency)
 
 else:
     print("Помилка!")
